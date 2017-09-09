6a6416a6696957c882be3df5b7f6d782163cda91
Robert Campbell <robert.campbell@elmolearning.com.au>
2017-09-09 16:41:05 +1000

    feature/test-views basic admin layout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,3 +23,25 @@ Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
 
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
+
+    Route::get('create', function () {
+        return view('admin.create');
+    })->name('admin.create');
+
+    Route::post('create', function() {
+        return "It works!";
+    })->name('admin.create');
+
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+
+    Route::post('edit', function() {
+        return "It works!";
+    })->name('admin.update');
+});
+
