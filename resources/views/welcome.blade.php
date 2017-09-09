417c221d016ed6d761a00c3e16f404da1617af7a
Robert Campbell <robert.campbell@elmolearning.com.au>
2017-09-09 15:12:16 +1000

    feature/test-views for loop outputting html from variable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,11 @@
         <div class="content">
             @include('partials.nav')
             <div class="title">Laravel 5</div>
+            <?php $var = '<p>some p</p>'; ?>
+            @for($i = 0; $i < 10; $i++ )
+                {!! $var !!}<!-- output html or js from variable -->
+            @endfor
+
         </div>
     </div>
 
