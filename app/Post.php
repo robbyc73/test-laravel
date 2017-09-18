2a53bad6828a376f1902c306005550b5110a54f5
Robert Campbell <robert.campbell@elmolearning.com.au>
2017-09-18 21:35:40 +1000

    feature/controllers-models all functions update create etc

--- app/Post.php
+++ app/Post.php
@@ -31,6 +31,47 @@ class Post
 
     /**
      * @param $session
+     * @param $id
+     * @return mixed
+     */
+    public function getPost($session, $id)
+    {
+        if (!$session->has('posts')) {
+            $this->createDummyData();
+        }
+        return $session->get('posts')[$id];
+    }
+
+    /**
+     * @param $session
+     * @param $title
+     * @param $content
+     */
+    public function addPost($session, $title, $content)
+    {
+        if (!$session->has('posts')) {
+            $this->createDummyData();
+        }
+        $posts = $session->get('posts');
+        array_push($posts, ['title' => $title, 'content' => $content]);
+        $session->put('posts', $posts);
+    }
+
+    /**
+     * @param $session
+     * @param $id
+     * @param $title
+     * @param $content
+     */
+    public function editPost($session, $id, $title, $content)
+    {
+        $posts = $session->get('posts');
+        $posts[$id] = ['title' => $title, 'content' => $content];
+        $session->put('posts', $posts);
+    }
+
+    /**
+     * @param $session
      */
     private function createDummyData($session)
     {
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,8 +6,8 @@
             <p class="quote">The beautiful Laravel</p>
         </div>
     </div>
-    @if(Session::has('posts'))
-        @foreach(Session::get('posts') as $key => $post)
+
+        @foreach($posts as $key => $post)
             <div class="row">
                 <div class="col-md-12 text-center">
                     <h1 class="post-title">{{ $post['title'] }}</h1>
@@ -15,9 +15,9 @@
                     <p><a href="{{ route('blog.post', ['id' => ($key+1)]) }}">Read more...</a></p>
                 </div>
             </div>
-            @if($key < count(Session::get('posts')) - 1)
+            @if($key < count($posts) - 1)
                 <hr>
             @endif
         @endforeach
-    @endif
+
 @endsection
